5c9a4e93a81e4ffbcc117010a42620e7722e8ae9
milord0212 <email>
2017-03-03 22:28:57 +0800

    new Pokemon

--- CS280-Lecturel-Examples/Program.cs
+++ CS280-Lecturel-Examples/Program.cs
@@ -10,7 +10,14 @@ namespace CS280_Lecturel_Examples
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello Word")
+            pokemon pokemon = new pokemon();
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 12.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            Console.WriteLine("Height:{0}, Weight:{1}",
+                pokemon.Height, pokemon.Weight);
         }
     }
 }
---
+++ CS280-Lecturel-Examples/pokemon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Lecturel_Examples
+{
+    internal class pokemon
+    {
+        public float Height;
+        public float Weight;
+        public string Abilities;
+        public string Category;
+        public string Gender;
+    }
+}
